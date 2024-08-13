2a81cec7900de20e6dc7086be8a5068a474b98d3
TEBY4 <felipeoliveramantero@gmail.com>
2024-08-13 13:09:06 -0400

    task

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo whois  $1 | awk '/^Tech/ || /^Admin/ || /^Registrant/ {gsub(" ",",");print}' > $1.csv
+sudo whois  $1 | awk -F': ' '/^Registrant/ || /^Admin/ || /^Tech/ {print $1 ", " $2}' > "$1.csv"
