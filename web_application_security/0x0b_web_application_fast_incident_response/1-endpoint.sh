9e3bdcece9d346635beb9c617b0ac1db645f6b07
TEBY4 <felipeoliveramantero@gmail.com>
2024-11-19 09:54:44 -0500

    task

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat logs.txt | grep -oE 'POST [^ ]+|GET [^ ]+' | awk '{print $2}' | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}'
+cat logs.txt | grep -oE '(POST|GET) .*' | awk '{print $2}' | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}'
