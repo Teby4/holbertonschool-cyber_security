1340ccb64afaf025d779f8c212a225f4a84b1a0a
Teby4 <felipeoliveramantero@gmail.com>
2024-11-23 20:15:33 -0500

    task

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep "Failed password\| Accepted password" | awk '{print$(NF-5)}' | uniq -c | sort -rn | head -n 1
+tail -n 1000 auth.log | grep "Failed password\| Accepted password" | awk '{print$(NF-5)}' | uniq -c | sort -rn | head -n 1 | awk '{print$2}'
